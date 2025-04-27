0f09bd70352858d5ce648a6901310d2ca57c338e
Henrique F. Teixeira <hriqueft@gmail.com>
2025-04-27 11:17:51 -0300

    add more plugins

--- templates/fullstack/app/app.rb
+++ templates/fullstack/app/app.rb
@@ -1,15 +1,24 @@
 class App < Roda
   # PLUGINS
-  #plugin :hash_branch_view_subdir
-  plugin :autoload_hash_branches
-  autoload_hash_branch_dir('./app/routes')
 
+  # General plugins
+  plugin :autoload_hash_branches
   plugin :json
+  plugin :json_parser
+  plugin :all_verbs
+  plugin :common_logger
+  plugin :halt
+
+  # View plugins
   plugin :render, views: 'app/views'
+  plugin :partials, views: 'app/views'
+  plugin :content_for, append: false
 
   plugin :not_found do
     'not found'
   end
+
+  autoload_hash_branch_dir('./app/routes')
 
   route do |r|
     r.hash_branches
