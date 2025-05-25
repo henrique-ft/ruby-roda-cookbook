4cca70397f83a3b011553684b084c8186de25017
Henrique F. Teixeira <hriqueft@gmail.com>
2025-05-24 21:52:39 -0300

    connect to postgres

--- templates/fullstack/app/db/conn.rb
+++ templates/fullstack/app/db/conn.rb
@@ -1,7 +1,14 @@
 module DB
   class Conn
     def self.init
-      @conn = Sequel.connect("sqlite://db/#{ENV['RACK_ENV'] || 'development'}.db")
+      #@conn = Sequel.connect("sqlite://db/#{ENV['RACK_ENV'] || 'development'}.db")
+      @conn =
+        Sequel.postgres(
+          database: 'app_development',
+          host: 'localhost',
+          user: 'dev',
+          password: 'dev'
+        )
 
       Sequel::Model.plugin :timestamps, update_on_create: true
     end
